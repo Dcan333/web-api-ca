10d1ad7136064850734857b55e7470742e4c9ded
Daniel Canning <canning932@gmail.com>
2025-05-25 11:31:29 +0100

    broken

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,11 @@ import './db';
 import cors from 'cors';
 import usersRouter from './api/users';
 import authenticate from './authenticate';
+import moviesRouter from './api/movies';   
+
+
+
+
 
 
 dotenv.config();
@@ -27,6 +32,8 @@ const port = process.env.PORT;
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/api/movies', moviesRouter); 
 
 app.use('/api/users', usersRouter);
 
