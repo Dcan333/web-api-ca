596f1aa9de7e06b28405d2fc87a71affcf6bbfc0
Daniel Canning <canning932@gmail.com>
2025-05-25 14:32:51 +0100

    added the routes

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import {
+    getMovies, getUpcomingMovies,
+    getGenres,
+    getPopularMovies,
+    getTopRatedMovies,
+    getNowPlayingMovies
+} from '../tmdb-api';
 
 
 const router = express.Router();
@@ -10,5 +16,29 @@ router.get('/discover', asyncHandler(async (req, res) => {
     res.status(200).json(discoverMovies);
 }));
 
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
+router.get('/genres', asyncHandler(async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
+}));
+
+router.get('/popular', asyncHandler(async (req, res) => {
+    const popularMovies = await getPopularMovies();
+    res.status(200).json(popularMovies);
+}));
+
+router.get('/top-rated', asyncHandler(async (req, res) => {
+    const topRatedMovies = await getTopRatedMovies();
+    res.status(200).json(topRatedMovies);
+}));
+
+router.get('/now-playing', asyncHandler(async (req, res) => {
+    const nowPlayingMovies = await getNowPlayingMovies();
+    res.status(200).json(nowPlayingMovies);
+}));
 
 export default router;
